830f338bc7381d52ccc291953c01327640ecc7bf
Apriadi <apripeni15@gmail.com>
2021-05-18 19:03:37 +0700

    membuat model dan migrasi SubTaks

---
+++ src/migrations/20210518120033-create-sub-taks.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('SubTaks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      taksId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Taks', key: 'id' }
+      },
+      subTaksName: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('SubTaks');
+  }
+};
